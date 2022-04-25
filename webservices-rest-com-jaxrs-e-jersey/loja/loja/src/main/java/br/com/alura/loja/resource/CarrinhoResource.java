57726343d4c6fef834513ea1388fa1d6b378d04b
INDRA\snepomuceno <BELI@17712>
2022-04-25 14:50:35 -0300

    Módulo 11: "Atualizando recursos e o PUT". Nessa fase aprendemos como atualizar registros, via PUT.

--- webservices-rest-com-jaxrs-e-jersey/loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ webservices-rest-com-jaxrs-e-jersey/loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,6 +18,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -60,5 +62,28 @@ public class CarrinhoResource {
          carrinho.remove(produtoId); // chamar metodo do banco que exluirá
     	return Response.ok().build();
 
+    }
+    
+  //Metodo de atualização
+    //ALTERA APENAS 1 PARAMETRO DO PRODUTO
+    @Path("{id}/produtos/{produtoId}/quantidade")
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response alteraProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId, String conteudo) {
+        Carrinho carrinho = new CarrinhoDAO().busca(id);
+        Produto produto = (Produto) new XStream().fromXML(conteudo);//O que enviaremos para alterar
+        carrinho.trocaQuantidade(produto);
+        return Response.ok().build();
+    }
+ //Metodo de atualização
+    //ALTERA O PRODUTO INTEIRO
+    @Path("{id}/produtos/{produtoId}")
+    @PUT
+    public Response alteraProdutoInteiro(@PathParam("id") long id,
+            @PathParam("produtoId") long produtoId, String conteudo) {
+        Carrinho carrinho = new CarrinhoDAO().busca(id);
+        Produto produto = (Produto) new XStream().fromXML(conteudo);
+        carrinho.troca(produto);
+        return Response.ok().build();
     }
 }
